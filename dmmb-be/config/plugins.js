c2dd677ce09efd9b3f0d46f74a8f05c1b4c7ad16
Milan Blaško <milan.blasko@interway.sk>
2023-11-16 07:42:19 +0100

    upravy

--- dmmb-be/config/plugins.js
+++ dmmb-be/config/plugins.js
@@ -1,16 +1,22 @@
 module.exports = ({ env }) => ({
   // ...
-  email: {
-    config: {
-      provider: 'sendgrid',
-      providerOptions: {
-        apiKey: env('SENDGRID_API_KEY'),
-      },
-      settings: {
-        defaultFrom: 'milanblasko28@gmail.com',
-        defaultReplyTo: 'milanblasko28@gmail.com',
-      },
-    },
-  },
+ email: {
+		config: {
+			provider: 'nodemailer',
+			providerOptions: {
+				host: env('SMTP_HOST'),
+				port: env('SMTP_PORT'),
+				auth: {
+					user: env('SMTP_USERNAME'),
+					pass: env('SMTP_PASSWORD'),
+				},
+			},
+			settings: {
+				defaultFrom: 'milanblasko28@gmail.com',
+				defaultReplyTo: 'milanblasko28@gmail.com',
+				testAddress: 'milanblasko28@gmail.com',
+			},
+		},
+	},
   // ...
 });
